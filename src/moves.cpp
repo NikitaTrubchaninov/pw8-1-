dfd48ce23c12727a8b73303f330f3fe1e5e404da
Трубчанінов Нікіта <nikita.ochakov@gmail.com>
2025-06-21 19:58:36 +0300

    cpp files

---
+++ src/moves.cpp
@@ -0,0 +1,37 @@
+#include "moves.hpp"
+#include "board.hpp"
+#include <iostream>
+
+
+int move1() {
+    int move;
+    std::cout << "enter num (1-9): ";
+    std::cin >> move;
+    while (move < 1 || move > 9 || h1[move] != '-') {
+        std::cout << "incorrect, try again: ";
+        std::cin >> move;
+    }
+    return move;
+}
+
+int move2() {
+    int move;
+    std::cout << "enter num (1-16): ";
+    std::cin >> move;
+    while (move < 1 || move > 16 || h2[move] != '-') {
+        std::cout << "incorrect, try again: ";
+        std::cin >> move;
+    }
+    return move;
+}
+
+int move3() {
+    int move;
+    std::cout << "enter num (1-25): ";
+    std::cin >> move;
+    while (move < 1 || move > 25 || h3[move] != '-') {
+        std::cout << "incorrect, try again: ";
+        std::cin >> move;
+    }
+    return move;
+}
